3729c1755e3a5825f0e36f9f5e02b859702d383a
Ann Sokolova <fullsokan@gmail.com>
2021-11-02 19:59:50 +0300

    DE optimization (end)

--- differential_evolution_optimization.py
+++ differential_evolution_optimization.py
@@ -29,67 +29,109 @@ if __name__ == "__main__":
     t_max = 10
     left = -10
     right = 10
-    step = 0
+    step = 1
     # parameters
-    N = 5
+    n = 2
+    N = 10
     F = 0.5
-    P = 0.3
-    X = [[0] * N for i in range(3)]
-    # step 1
-    for i in range(0, N):
-        X[0][i] = random.uniform(left, right)
-        X[1][i] = random.uniform(left, right)
-        X[2][i] = -0.0001 * (abs(np.sin(X[0][i]) * np.sin(X[1][i]) * np.exp(abs(100 - (X[0][i] ** 2 + X[1][i] ** 2) ** 0.5 / np.pi))) + 1) ** 0.1
-    print(X)
+    P = 0.7
+    X = [0 for i in range(2 * N)]
+    X_M = [0, 0]
+    X_T = [0, 0]
+    T = []
 
-    # step 2
-    i = 0
+    while 1:
+        if step == 1:
+            # step 1
+            for i in range(0, 2 * N, 2):
+                X[i] = random.uniform(left, right)
+                X[i + 1] = random.uniform(left, right)
+            step = 2
 
-    # step 3 # error !!! ###############################################################################################
-    X_A = X[:][i]
-    X_B = X[:][i]
-    X_C = X[:][i]
-    print(len(X), len(X[0]))
-    print(X[0][0], X[1][0], X[2][0])
-    while X_A == X[:][i] and X_B == X[:][i] and X_C == X[:][i]:
-        temp_a = random.randint(0, len(X[0]))
-        temp_b = random.randint(0, len(X[0]))
-        temp_c = random.randint(0, len(X[0]))
-        X_A = X[:][temp_a]
-        X_B = X[:][temp_b]
-        X_C = X[:][temp_c]
-    print(X_A, X_B, X_C)
+        if step == 2:
+            # step 2
+            i = 0
+            step = 3
 
-    # step 4
+        if step == 3:
+            # step 3
+            X_A = X[0:3]
+            X_B = X_A[:]
+            X_C = X_A[:]
+            while X_A == X_B or X_A == X_C or X_B == X_C and X_A == X[i * 3:i * 3 + 3] and X_B == X[i * 3:i * 3 + 3] and X_C == X[i * 3:i * 3 + 3]:
+                temp_a = random.randint(0, N - 1)
+                temp_b = random.randint(0, N - 1)
+                temp_c = random.randint(0, N - 1)
+                X_A = X[temp_a * 2:temp_a * 2 + 2]
+                X_B = X[temp_b * 2:temp_b * 2 + 2]
+                X_C = X[temp_c * 2:temp_c * 2 + 2]
 
-    # step 5
-    k = 1
+            # step 4
+            X_M[0] = X_C[0] + F * (X_A[0] - X_B[0])
+            X_M[1] = X_C[1] + F * (X_A[1] - X_B[1])
 
-    # step 6
+            # step 5
+            k = 1
 
-    # step 7
+        # step 6
+        p = random.uniform(0, 1)
 
-    # step 8
-    k += 1
+        # step 7
+        if p < P:
+            X_T[k - 1] = X_M[k - 1]
+        else:
+            X_T[k - 1] = X[i * 2 + k - 1]
 
-    # step 9
+        # step 8
+        k += 1
 
-    # step 10
+        # step 9
+        if k <= n:
+            step = 6
+            continue
 
-    # step 11
+        # step 10
+        f_t = -0.0001 * (abs(np.sin(X_T[0]) * np.sin(X_T[1]) * np.exp(abs(100 - (X_T[0] ** 2 + X_T[1] ** 2) ** 0.5 / np.pi))) + 1) ** 0.1
+        f_i = -0.0001 * (abs(np.sin(X[i * 2]) * np.sin(X[i * 2 + 1]) * np.exp(abs(100 - (X[i * 2] ** 2 + X[i * 2 + 1] ** 2) ** 0.5 / np.pi))) + 1) ** 0.1
 
-    # step 12
-    i += 1
+        if f_t < f_i:
+            T.append(X_T[0])
+            T.append(X_T[1])
+            step = 12
+        else:
+            # step 11
+            T.append(X[i * 2])
+            T.append(X[i * 2 + 1])
 
-    # step 13
+        # step 12
+        i += 1
 
-    # step 14
+        # step 13
+        if i <= N - 1:
+            step = 3
+            continue
 
-    # step 15
+        # step 14
+        X = T
+        T = []
 
-    # step 16
+        # step 15
+        t = 0
+        for ii in range(0, N):
+            for jj in range(0, N):
+                if ii != jj:
+                    distance = np.linalg.norm(np.array(X[jj * 2:jj * 2 + 2]) - np.array(X[ii * 2:ii * 2 + 2]))
+                    if distance < e:
+                        t += 1
+        if t == N ** 2 - N:
+            X_M = X[0:2]
+            break
 
+        # step 16
+        step = 2
 
+    print(f"\n\t x_min = {X_M[0]} y_min = {X_M[1]} f_min = {-0.0001 * (abs(np.sin(X_M[0]) * np.sin(X_M[1]) * np.exp(abs(100 - (X_M[0] ** 2 + X_M[1] ** 2) ** 0.5 / np.pi))) + 1) ** 0.1}")
+    plt.scatter(X_M[0], X_M[1], c="b")
     t2 = time.time() - t1
     print(f"\n\tExecution time = {t2} seconds")
     plt.show()
